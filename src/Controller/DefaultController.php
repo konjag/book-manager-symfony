1bee8aadd27d03cab5f39e0c67f98a608db3e973
Konrad Jaguszewski <konrad.jaguszewski@gmail.com>
2018-12-19 18:45:46 +0100

    Add a list of borrowed books

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,8 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig', [
+            'books' => $this->getUser()->getBooks()
+        ]);
     }
 }
